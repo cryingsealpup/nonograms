1e98309760154a0224df05f4bb4d79397f252158
Ekaterina Radchenko <ekatherinered@gmail.com>
2024-02-06 02:45:29 +0400

    feat: prettier layout

--- src/js/index.js
+++ src/js/index.js
@@ -5,15 +5,23 @@ window.addEventListener('contextmenu', e => { e.preventDefault() }, false) // do
 
 let dim = 5
 const canvas = document.createElement('canvas'),
+	wrapper = document.createElement('div'),
+	buttonWrapper = document.createElement('div'),
 	body = document.querySelector('body'),
 	reset = document.createElement('button'),
 	solve = document.createElement('button')
 
-canvas.width = canvas.height = '470'
+
+canvas.width = canvas.height = window.innerWidth > 700 ? '470' : window.innerWidth
+window.addEventListener('resize', () => { location.reload() }) // Reload on resize to fit content
 canvas.textContent = 'Извините, ваш браузер не поддерживает "canvas" элемент.'
 reset.textContent = 'Reset'
 solve.textContent = 'Solve'
-body.append(canvas, reset, solve)
+wrapper.classList.add('wrapper')
+buttonWrapper.classList.add('button-wrapper')
+buttonWrapper.append(reset, solve)
+wrapper.append(canvas, buttonWrapper)
+body.append(wrapper)
 
 const nonogram = new Nonogram(canvas, dim)
 nonogram.play()
